a03066d15aa8993805925c7fc7a4818b7ca36d89
TippyToo <78093811+TippyToo@users.noreply.github.com>
2023-12-08 12:14:45 -0500

    Create minimaxsum.java

---
+++ minimaxsum.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+    // Write your code here
+    long min = arr.get(0);
+    long max = 0;
+    long sum = 0;
+    for (long n : arr) {
+        if (n < min) min = n;
+        if (n > max) max = n;
+        sum += n;
+    }
+    System.out.print((sum - max) + " " + (sum - min)); 
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
